6464bf590f6b8fa04fe094b18691e9b1fdf06d90
Domaoggy <85707302+Domaoggy@users.noreply.github.com>
2021-07-05 12:26:48 +0300

    Add files via upload

---
+++ Array/Array/source.c
@@ -0,0 +1,11 @@
+/*Details for Ogembo Marabu in c*/
+#include <stdio.h> 
+int main()
+{
+    printf("Name   : Ogembo Marabu Douglas\n");
+    printf("DOB    : May 22, 1993\n");
+    printf("Mobile : +254725052926\n");
+    printf("Stree Address: Langata Rongai-Olerai-Max Appartment\n");
+
+    return (0);
+}
